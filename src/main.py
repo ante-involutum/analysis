d7c7dfffe98a43f9b4bb7806a709d9b2f1a02c1d
lunz1207 <lunz1207@gmail.com>
2022-06-26 15:18:28 +0800

    feat: action & metrics

--- src/main.py
+++ src/main.py
@@ -28,12 +28,20 @@ query_api = client.query_api()
 @app.post("/analysis")
 async def file_upload():
 
-    query = 'from(bucket: "primary") \
-    |> range(start: -420m) \
+    query = f'from(bucket: "primary") \
+    |> range(start: -10s) \
+    |> filter(fn: (r) => r["_measurement"] == "jmeter") \
+    |> filter(fn: (r) => r["_field"] == "avg" or r["_field"] == "count" or r["_field"] == "countError" or r["_field"] == "hit" or r["_field"] == "endedT" or r["_field"] == "max" or r["_field"] == "maxAT" or r["_field"] == "meanAT" or r["_field"] == "minAT" or r["_field"] == "min" or r["_field"] == "pct90.0" or r["_field"] == "pct95.0" or r["_field"] == "rb" or r["_field"] == "pct99.0" or r["_field"] == "startedT" or r["_field"] == "sb") \
     |> filter(fn: (r) => r["application"] == "kakax") \
-    |> filter(fn: (r) => r["_measurement"] == "events" or r["_measurement"] == "jmeter") \
-    |> filter(fn: (r) => r["_field"] == "avg") \
+    |> filter(fn: (r) => r["statut"] == "all") \
+    |> filter(fn: (r) => r["transaction"] == "all") \
     |> yield(name: "mean")'
 
-    result = query_api.query(query)[0].records[0].values.items()
-    return result
+    result = query_api.query(query)
+
+    resp = {}
+    for i in result:
+        a = i.records[-1].values
+        resp[a['_field']] = a['_value']
+    print(resp)
+    return resp
