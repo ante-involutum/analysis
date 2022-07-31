12b959aa64fedd91f28de9f8cca5ff778a45bc02
lunz1207 <lunz1207@gmail.com>
2022-07-31 11:20:16 +0800

    feat: support kafka & prometheus

--- src/main.py
+++ src/main.py
@@ -1,47 +1,53 @@
 from fastapi import FastAPI
-
+from fastapi.responses import PlainTextResponse
 from fastapi.middleware.cors import CORSMiddleware
-from influxdb_client import InfluxDBClient
-from src.config import TOKEN, HOST, PORT
+
+from src.env import *
+import prometheus_client
+from kafka import KafkaConsumer
+from src.metrics import registry, jmeter
+from src.helper import msg_to_dict
+
 
 app = FastAPI(name="analysis")
 
-origins = [
-    # "http://localhost",
-    "*"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["http://localhost", "*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
-client = InfluxDBClient(
-    url=f"http://{HOST}:{PORT}", token=TOKEN, org='primary')
-query_api = client.query_api()
+origin_consumer = KafkaConsumer(
+    'jmx',
+    group_id='origin',
+    bootstrap_servers=['middleware-kafka.tink:9092'],
+    auto_offset_reset='earliest'
+)
+
+metrics_consumer = KafkaConsumer(
+    'jmx',
+    group_id='metrics',
+    bootstrap_servers=['middleware-kafka.tink:9092'],
+    auto_offset_reset='earliest',
+)
 
 
-@app.post("/analysis")
-async def file_upload():
+@app.get('/metrics', response_class=PlainTextResponse)
+def get_data():
+    msg = next(metrics_consumer)
+    value = eval(msg.value.decode("UTF-8"))['message']
 
-    query = f'from(bucket: "primary") \
-    |> range(start: -10s) \
-    |> filter(fn: (r) => r["_measurement"] == "jmeter") \
-    |> filter(fn: (r) => r["_field"] == "avg" or r["_field"] == "count" or r["_field"] == "countError" or r["_field"] == "hit" or r["_field"] == "endedT" or r["_field"] == "max" or r["_field"] == "maxAT" or r["_field"] == "meanAT" or r["_field"] == "minAT" or r["_field"] == "min" or r["_field"] == "pct90.0" or r["_field"] == "pct95.0" or r["_field"] == "rb" or r["_field"] == "pct99.0" or r["_field"] == "startedT" or r["_field"] == "sb") \
-    |> filter(fn: (r) => r["application"] == "kakax") \
-    |> filter(fn: (r) => r["statut"] == "all") \
-    |> filter(fn: (r) => r["transaction"] == "all") \
-    |> yield(name: "mean")'
+    for k, v in msg_to_dict(value).items():
+        jmeter.labels('jmeter', msg.topic, k).set(v)
 
-    result = query_api.query(query)
+    return prometheus_client.generate_latest(registry)
 
-    resp = {}
-    for i in result:
-        a = i.records[-1].values
-        resp[a['_field']] = a['_value']
-    print(resp)
-    return resp
+
+@app.get('/analysis/kafak')
+def get_kafak_data():
+    msg = next(origin_consumer)
+    result = eval(msg.value.decode("UTF-8"))
+    return result
