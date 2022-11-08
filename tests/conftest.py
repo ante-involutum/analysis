ee6cb9c13441efa6b2fcaa2539415386ef1f0701
lunz1207 <lunz1207@gmail.com>
2022-11-08 14:10:57 +0800

    chore: case env

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,10 +5,10 @@ from requests_toolbelt.sessions import BaseUrlSession
 
 
 TEST_ENV = os.getenv('TEST_ENV')
-if TEST_ENV == 'local':
+if TEST_ENV == 'dev':
     url = 'http://127.0.0.1:8005'
     ws_url = "ws://127.0.0.1:8005/analysis/ws/raw"
-elif TEST_ENV == 'apisix':
+elif TEST_ENV == 'test':
     url = 'http://tink.test:31695'
     ws_url = "ws://tink.test:31695/analysis/ws/raw"
 
