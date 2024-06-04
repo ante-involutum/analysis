65e59cfca500271025eda9091578c9f2f983f047
lunz1207 <lunz1207@gmail.com>
2024-06-04 18:13:12 +0800

    feat: refactor case

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,37 +1,33 @@
 import json
 import pytest
 import websocket
-from pprint import pprint
 
 
-@pytest.mark.usefixtures('init')
-class TestAnalysis():
+@pytest.mark.usefixtures("init")
+class TestAnalysis:
 
     payload = {
-        'index': 'logs',
-        'key_words': {
-            'pod.name': 'qingcloud-autotest-2951-5aa237ae-7d6e-46a4-bbd6-423110ea0231',
-            'container.name': 'aomaker',
-            # 'kubernetes.labels.uid': '091143e5-464e-4704-8438-04ecc98f4b1a',
+        "index": "logs",
+        "key_words": {
+            "pod.name": "qingcloud-autotest-task4557",
+            "container.name": "aomaker",
         },
         "from_": 0,
         "size": 200,
+        # "offset": [1717113612433],
+        # "offset": [1717116041813],
     }
 
-    def test_es_log(self):
-        resp = self.bs.post(
-            f'{self.url}/analysis/raw',
-            json=self.payload
-        )
-        pprint(resp.json())
+    def test_http_log(self):
+        resp = self.bs.post(f"{self.url}/analysis/raw", json=self.payload)
         assert resp.status_code == 200
 
-        self.payload['offset'] = resp.json()['offset']
+    def test_ws_log(self):
         ws = websocket.WebSocket()
-        ws.connect(
-            f'{self.ws_url}/analysis/ws/raw',
-        )
-        ws.send(json.dumps(self.payload))
-        resp = ws.recv()
-        pprint(json.loads(resp))
-        assert ws.status == 101
+        ws.connect(f"{self.ws_url}/analysis/ws/raw")
+        while True:
+            ws.send(json.dumps(self.payload))
+            resp = ws.recv()
+            assert ws.status == 101
+            resp = json.loads(resp)
+            self.payload['offset'] = resp.get('offset')
